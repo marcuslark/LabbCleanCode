d15312d6a4c522918d48b9b1ac7676274b79b55f
Marcus Lärk <marcus.lark.stahlberg@iths.se>
2021-11-02 14:51:30 +0100

    refactored and extracted method in Universe.java to clean upp code

--- src/main/java/se/iths/labb/Universe.java
+++ src/main/java/se/iths/labb/Universe.java
@@ -1,4 +1,5 @@
 package se.iths.labb;
+
 public class Universe {
 
     private Cell[][] state;
@@ -6,9 +7,9 @@ public class Universe {
     public Universe(Cell.cellState[][] cellStates) {
         state = new Cell[cellStates.length][];
 
-        for(int row=0; row<cellStates.length; row++) {
+        for (int row = 0; row < cellStates.length; row++) {
             state[row] = new Cell[cellStates[row].length];
-            for(int col=0; col<cellStates[row].length; col++) {
+            for (int col = 0; col < cellStates[row].length; col++) {
                 state[row][col] = new Cell(cellStates[row][col]);
             }
         }
@@ -17,9 +18,9 @@ public class Universe {
     public Cell.cellState[][] getState() {
         Cell.cellState[][] cellStates = new Cell.cellState[state.length][];
 
-        for(int row=0; row<state.length; row++) {
+        for (int row = 0; row < state.length; row++) {
             cellStates[row] = new Cell.cellState[state[row].length];
-            for(int col=0; col<state[row].length; col++) {
+            for (int col = 0; col < state[row].length; col++) {
                 cellStates[row][col] = state[row][col].getState();
             }
         }
@@ -28,47 +29,49 @@ public class Universe {
 
     public void update() {
         Cell.cellState[][] cellStates = getState();
-        for(int row=0; row<state.length; row++) {
-            for(int col=0; col<state[row].length; col++) {
+        for (int row = 0; row < state.length; row++) {
+            for (int col = 0; col < state[row].length; col++) {
                 int numberOfAliveNeighbours = getNumberOfAliveNeighbours(cellStates, row, col);
-               state[row][col].update(numberOfAliveNeighbours);
+                state[row][col].update(numberOfAliveNeighbours);
             }
         }
     }
 
-    private int getNumberOfAliveNeighbours(Cell.cellState[][] state,int row, int col) {
+    private int getNumberOfAliveNeighbours(Cell.cellState[][] state, int row, int col) {
         int numberOfAliveNeighbours = 0;
-        if(row>0) {
-            int rowAbove = row -1;
-            if(col > 0) {
-                if(state[rowAbove][col -1] == Cell.cellState.ALIVE) numberOfAliveNeighbours++;
-            }
-            if (state[rowAbove][col] == Cell.cellState.ALIVE) numberOfAliveNeighbours++;
-            if (col<state[row].length-1) {
-                if(state[rowAbove][col+1] == Cell.cellState.ALIVE) numberOfAliveNeighbours++;
-            }
+        if (row > 0) {
+            int rowAbove = row - 1;
+            numberOfAliveNeighbours += getNumberOfAliveNeighboursInRow(state, row, col, rowAbove);
         }
 
-        if(col > 0) {
-            if(state[row][col -1] == Cell.cellState.ALIVE) numberOfAliveNeighbours++;
+        if (col > 0) {
+            if (state[row][col - 1] == Cell.cellState.ALIVE)
+                numberOfAliveNeighbours++;
         }
-        if (col<state[row].length-1) {
-            if(state[row][col+1] == Cell.cellState.ALIVE) numberOfAliveNeighbours++;
+        if (col < state[row].length - 1) {
+            if (state[row][col + 1] == Cell.cellState.ALIVE)
+                numberOfAliveNeighbours++;
         }
-
-        if(row<state.length -1) {
+        if (row < state.length - 1) {
             int rowBelow = row + 1;
-            if(col > 0) {
-                if(state[rowBelow][col -1] == Cell.cellState.ALIVE) numberOfAliveNeighbours++;
-            }
-            if (state[rowBelow][col] == Cell.cellState.ALIVE) numberOfAliveNeighbours++;
-            if (col<state[row].length-1) {
-                if(state[rowBelow][col+1] == Cell.cellState.ALIVE) numberOfAliveNeighbours++;
-            }
+                numberOfAliveNeighbours += getNumberOfAliveNeighboursInRow(state, row, col, rowBelow);
+        }
+        return numberOfAliveNeighbours;
+    }
 
-        }   return numberOfAliveNeighbours;
-
-
+    private int getNumberOfAliveNeighboursInRow(Cell.cellState[][] state, int row, int col, int rowAbove) {
+        int numberOfAliveNeighbours = 0;
+        if (col > 0) {
+            if (state[rowAbove][col - 1] == Cell.cellState.ALIVE)
+                numberOfAliveNeighbours++;
+        }
+        if (state[rowAbove][col] == Cell.cellState.ALIVE)
+            numberOfAliveNeighbours++;
+        if (col < state[row].length - 1) {
+            if (state[rowAbove][col + 1] == Cell.cellState.ALIVE)
+                numberOfAliveNeighbours++;
+        }
+        return numberOfAliveNeighbours;
     }
 
 }
